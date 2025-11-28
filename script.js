43c0b0474ab82f1835b08dffb40a34808e9b6496
Faizan Ahmad <faizishiekh9998@gmail.com>
2025-11-28 14:26:17 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example JS for button actions
+
+document.querySelectorAll(".btn").forEach(btn => {
+    btn.addEventListener("click", () => {
+        alert("Demo button clicked — attach real function later!");
+    });
+});
